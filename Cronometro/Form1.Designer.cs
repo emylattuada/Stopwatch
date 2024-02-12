ec246c76736991940bf01aa25bfc0a5921d90051
emylattuada <emjuan1234@gmail.com>
2024-02-12 05:16:10 -0300

    Primer diseño de form

--- Cronometro/Form1.Designer.cs
+++ Cronometro/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Cronometro
 {
-    partial class Form1
+    partial class formPrincipal
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,19 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formPrincipal));
+            panelBar = new Panel();
+            labelBar = new Label();
+            picLogoBar = new PictureBox();
+            panelBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)picLogoBar).BeginInit();
             SuspendLayout();
             // 
-            // Form1
+            // panelBar
+            // 
+            panelBar.BackColor = SystemColors.ActiveCaptionText;
+            panelBar.Controls.Add(labelBar);
+            panelBar.Controls.Add(picLogoBar);
+            panelBar.Location = new Point(0, 0);
+            panelBar.Name = "panelBar";
+            panelBar.Size = new Size(331, 31);
+            panelBar.TabIndex = 0;
+            panelBar.Paint += panelBar_Paint;
+            // 
+            // labelBar
+            // 
+            labelBar.AutoSize = true;
+            labelBar.BackColor = Color.Transparent;
+            labelBar.Enabled = false;
+            labelBar.ForeColor = Color.White;
+            labelBar.Location = new Point(26, 8);
+            labelBar.Name = "labelBar";
+            labelBar.Size = new Size(72, 15);
+            labelBar.TabIndex = 1;
+            labelBar.Text = "Cronómetro";
+            // 
+            // picLogoBar
+            // 
+            picLogoBar.BackColor = Color.Transparent;
+            picLogoBar.Image = (Image)resources.GetObject("picLogoBar.Image");
+            picLogoBar.Location = new Point(3, 4);
+            picLogoBar.Name = "picLogoBar";
+            picLogoBar.Size = new Size(23, 22);
+            picLogoBar.SizeMode = PictureBoxSizeMode.Zoom;
+            picLogoBar.TabIndex = 0;
+            picLogoBar.TabStop = false;
+            // 
+            // formPrincipal
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(439, 182);
-            Name = "Form1";
+            ClientSize = new Size(331, 132);
+            Controls.Add(panelBar);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "formPrincipal";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Cronómetro";
+            panelBar.ResumeLayout(false);
+            panelBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)picLogoBar).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panelBar;
+        private PictureBox picLogoBar;
+        private Label labelBar;
     }
 }
--- Cronometro/Form1.cs
+++ Cronometro/Form1.cs
@@ -4,15 +4,38 @@ using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 namespace Cronometro
 {
-    public partial class Form1 : Form
+    public partial class formPrincipal : Form
     {
 
-        public Form1()
+        public formPrincipal()
         {
             InitializeComponent();
-           
+
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            LinearGradientBrush gradientBrush = new LinearGradientBrush(
+                new Point(0, 0),
+                new Point(ClientSize.Width, ClientSize.Height),
+                Color.FromArgb(3, 239, 254),
+                Color.FromArgb(78, 173, 254));
+
+            e.Graphics.FillRectangle(gradientBrush, ClientRectangle);
         }
 
-       
+        private void panelBar_Paint(object sender, PaintEventArgs e)
+        {
+            LinearGradientBrush gradientBrush = new LinearGradientBrush(
+                new Point(0, 0), // Punto de inicio del degradado (izquierda superior)
+                new Point(panelBar.ClientSize.Width, panelBar.ClientSize.Height), // Punto final del degradado (derecha inferior)
+                Color.FromArgb(23, 23, 23), // Color inicial
+                Color.FromArgb(13, 13, 13)); // Color final
+
+            // Dibuja el degradado en el área del panel.
+            e.Graphics.FillRectangle(gradientBrush, panelBar.ClientRectangle);
+        
+    }
     }
 }
