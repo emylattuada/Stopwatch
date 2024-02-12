62cb9f3a33fc7bbcf9b4da5db019d6bd34a4b91d
Administrador <Administrador@DESKTOP-86Q74ME>
2024-02-12 04:51:12 -0300

    Agregar archivos de proyecto.

---
+++ Cronometro/Form1.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+namespace Cronometro
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+       
+    }
+}
